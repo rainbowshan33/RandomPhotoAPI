c3eaadc25c3fb9d6e9acfffa2529de63bcd13d31
rainbowshan33 <rainbowshan33@gmail.com>
2021-09-23 10:34:36 +0800

    Initial Commit

---
+++ RandomPhotoAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhotoAPI
+//
+//  Created by 王冊 on 2021/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
