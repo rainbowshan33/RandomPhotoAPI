c3ac2dbecebb5dbe788f0ef17145dd0ac294cc31
rainbowshan33 <rainbowshan33@gmail.com>
2021-09-23 15:06:53 +0800

    commit

--- RandomPhotoAPI/ViewController.swift
+++ RandomPhotoAPI/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    func fetchImage() {
+        let urlStr = "https://picsum.photos/400/400"
+        
+        if let url = URL(string: urlStr) {
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let data =  data {
+                    DispatchQueue.main.async {
+                        self.imageView.image = UIImage(data: data)
+                    }
+                }
+            }.resume()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            fetchImage()
     }
 
+    @IBAction func changeImage(_ sender: UIButton) {
+        fetchImage()
+    }
 
 }
-
